cf24e279ef84411b28f983d3cf261d2bdff873ec
coocloud <apple19903@gmail.com>
2019-05-15 13:10:23 +0200

    add format-timestamp helper

--- app/helpers/format-timestamp.js
+++ app/helpers/format-timestamp.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
+import { dateToString } from 'shlack/utils/date';
 
-export function formatTimestamp(params/*, hash*/) {
-  return params;
-}
-
-export default helper(formatTimestamp);
+export default helper(function formatTimestamp(params/*, hash*/) {
+  const [dateIsh] = params;
+  return dateToString(dateIsh);
+});
--- tests/integration/helpers/format-timestamp-test.js
+++ tests/integration/helpers/format-timestamp-test.js
@@ -8,10 +8,10 @@ module('Integration | Helper | format-timestamp', function(hooks) {
 
   // Replace this with your real tests.
   test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+    this.set('myDate', '01-01-1989');
 
-    await render(hbs`{{format-timestamp inputValue}}`);
+    await render(hbs`{{format-timestamp myDate}}`);
 
-    assert.equal(this.element.textContent.trim(), '1234');
+    assert.equal(this.element.textContent.trim(), 'Jan 1, 1989 00:00.00 AM');
   });
 });
