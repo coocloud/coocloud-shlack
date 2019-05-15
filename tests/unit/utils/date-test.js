e57275e6ef8ffa394993672d06d43d487bc2cf08
coocloud <apple19903@gmail.com>
2019-05-15 13:05:20 +0200

    ember g util date

---
+++ tests/unit/utils/date-test.js
@@ -0,0 +1,11 @@
+import date from 'coocloudshlack/utils/date';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | date', function() {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = date();
+    assert.ok(result);
+  });
+});
