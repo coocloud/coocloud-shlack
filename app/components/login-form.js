6fd1d39f7d419161886cb8bb2416d4dfae4d3bd8
coocloud <apple19903@gmail.com>
2019-05-16 10:33:14 +0200

    add login action

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,4 +1,20 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class LoginFormComponent extends Component {
+  loginAsUserWithId(val) {
+    console.log('UserId: ', val);
+  }
+
+  /**
+   *
+   * @param {Event & {target: HTMLFormElement}} evt
+   */
+  @action
+  onLoginFormSubmit(evt) {
+    evt.preventDefault();
+    const { target } = evt;
+    const val = target.querySelector('select').value;
+    this.loginAsUserWithId(val);
+  }
 }
